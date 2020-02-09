08642260cc420b82c4990b4c6531e94cd8b131cc
Oby Manyando <33107568+obymanyando@users.noreply.github.com>
2020-02-09 14:01:51 +0200

    started building display blocks with JS

--- js/shopCat.js
+++ js/shopCat.js
@@ -5,10 +5,12 @@ gridDisplay = document.querySelector('.grid__display')
 // blockImage = document.querySelector('.block__image')
 // blockText = document.querySelector('.block__text')
 // className = document.setAttribute('class', 'name')
+let blocks = ''
 
-
-const buildBlocks = () => {
-    for (let i = 0; i < 12; i++) {
+const buildBlocks = (blocks) => {
+    let i = blocks
+    console.log(blocks)
+    for (i = 0; i < 12; i++) {
         displayBlock = document.createElement('div')
         blockImage = document.createElement('IMG')
         blockText = document.createElement('div')
@@ -25,9 +27,44 @@ const buildBlocks = () => {
         blockText.appendChild(blockTextUp)
         blockText.appendChild(blockTextDown)
         gridDisplay.appendChild(displayBlock)
-        gridWrapper.appendChild(gridDisplay)
-        catContainer.append(gridWrapper)
+        console.log(blocks[2])
+        for (j = 0; j < 2; j++) {
+            blockTextDown.style.color = 'red'
+        }
     }
 
+
+}
+buildBlocks(blocks)
+console.log(buildBlocks(blocks))
+
+
+
+// innerHTML version
+/* 
+let blocks = ''
+
+const buildBlocks = () => {
+    for (let i = 0; i < 12; i++) {
+        blocks += `<div class = "display__block">
+                        <div class="block__image">
+                            <a href="https://placeholder.com"><img
+                            src="https://via.placeholder.com/200" alt="square" class="image__inner"></a>
+                        </div>
+                        <div class="block__text">
+                            <div class="text__desc">
+                            <P>sleeve spring suite</P>
+                            </div>
+                            <div class="text__price">
+                                <p>€864.00</p>
+                            </div>
+                        </div>
+
+                    </div>`
+
+    }
+    gridDisplay.innerHTML = blocks
+    console.log(div[1])
 }
 buildBlocks()
+*/
