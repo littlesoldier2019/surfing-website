21f6c4b149aea8afb5dfe06fe19e60ef909dd0f8
CPark <parkchaeah331@gmail.com>
2020-02-12 17:58:35 +0200

    slider with glide js

--- js/product-detail.js
+++ js/product-detail.js
@@ -1,6 +1,8 @@
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
+// More info tab
+
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
