ee21d892a947d66d51476f48b0aaf95cb46fddda
CPark <parkchaeah331@gmail.com>
2020-02-13 15:44:37 +0200

    create a modal for adding review function

--- js/product-detail.js
+++ js/product-detail.js
@@ -2,7 +2,6 @@ const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
 // More info tab
-
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
@@ -17,9 +16,35 @@ tabs.forEach(tab => {
   })
 })
 
+// Rating 
+//1. When a user click Reviews, show the review section
 
-// const sliders = document.querySelectorAll(".swiper-container");
+//2. Add review modal (let user add a review)
+const modal = document.getElementById("rating__modal");
+const modalBtn = document.getElementById("addReview__btn");
+const closeBtn = document.getElementById("closeBtn");
+const openModal = () => {
+  modal.style.display = "block";
+}
+const closeModal = () => {
+  modal.style.display = "none";
+}
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener("click", closeModal);
 
-// sliders.forEach(slider => {
-//   new Swiper(slider, config);
-// });
+//Form
+// 1. Size
+// - When a user click the size, set it as the size value
+// 2. Length
+// - When a user clikc the length, set the choice as the length value
+// 3. Quantity
+//- User can change the quantity through 1) typing number or 2) clicking the plus button 
+// 4. Add to cart
+//- When click the button, all form information save to the local storage
+// 5. Add to wishList
+// 6. Add to compare
+// 7. Email to a friend
+
+//Event 
+
+console.log("working on a modal style. customize input fields")
