190539c0aaf0819c4acdbe4e645c4c1186989d27
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-02-13 15:31:31 +0200

    footer

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-// // Initialize and add the map
+// Initialize and add the map
 // function initMap() {
 //     // The location of Integrify
 //     var Integrify = {lat: 60.170407, lng: 24.923704};
@@ -9,3 +9,38 @@
 //     var marker = new google.maps.Marker({position: Integrify, map: map});
 //   }  
 
+window.addEventListener('load', () => {
+
+
+let fullName = document.getElementById('full-name');
+// let email = document.getElementById('email');
+// let company = document.getElementById('company');
+// let subject = document.getElementById('subject');
+// let message = document.getElementById('message');
+let errorText = document.getElementById('errorText');
+
+
+let form = document.getElementById('form');
+
+    form.addEventListener('submit', (e) => {
+
+        let errorArr = [];
+
+        if (!fullName.value.match(/^[a-zA-Z]$/)) {
+            errorArr.push('Please use normal letter');
+        }
+
+        if (fullName.value.length < 2)
+            errorArr.push('Name is too short');
+
+        if (fullName.value.length > 20)
+        errorArr.push('Name is too long');
+
+        if (errorArr.length > 0) {
+            e.preventDefault();
+            errorText.innerHTML = errorArr.join('. ')
+        }
+
+    })
+
+})
