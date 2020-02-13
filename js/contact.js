53bf1066832af92d7c140458fe18af05a027785a
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-02-13 13:43:53 +0200

    form validation

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,11 @@
-// Initialize and add the map
-function initMap() {
-    // The location of Integrify
-    var Integrify = {lat: 60.170407, lng: 24.923704};
-    // The map, centered at Integrify
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: Integrify});
-    // The marker, positioned at Integrify
-    var marker = new google.maps.Marker({position: Integrify, map: map});
-  }  
+// // Initialize and add the map
+// function initMap() {
+//     // The location of Integrify
+//     var Integrify = {lat: 60.170407, lng: 24.923704};
+//     // The map, centered at Integrify
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 15, center: Integrify});
+//     // The marker, positioned at Integrify
+//     var marker = new google.maps.Marker({position: Integrify, map: map});
+//   }  
+
