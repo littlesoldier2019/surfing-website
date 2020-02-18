06ab04ecfc6b1613f734c86b5ea23141e1d816e3
stbugyei <stbugyei@yahoo.co.uk>
2020-02-18 13:56:08 +0200

    slider indicator changed to rectangle

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 /*let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex);*/
 
 
 //================ Next/previous controls =============
-function navSlides(n) {
+/*function navSlides(n) {
     showSlides(slideIndex += n);
-}
+}*/
 
 
 //================ Thumbnail image controls ===========
 
-function thumbnailSlide(n) {
+/*function thumbnailSlide(n) {
     showSlides(slideIndex = n);
-}
+}*/
 
 
-function showSlides(n) {
+/*function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName('banner_slide--Imgcontent');
-    let dots = document.getElementsByClassName('dot');
+    let rectangle = document.getElementsByClassName('dot');
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < rectangle.length; i++) {
+        rectangle[i].className = rectangle[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    rectangle[slideIndex - 1].className += " active";
 }
-showSlides()*/
+showSlides() */
 
 //================= Auto changing of slides ====================
 let slideIndex = 0;
@@ -38,17 +38,17 @@ let slideIndex = 0;
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName('banner_slide--Imgcontent');
-    let dots = document.getElementsByClassName('dot');
+    let rectangle = document.getElementsByClassName('rectangle');
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < rectangle.length; i++) {
+        rectangle[i].className = rectangle[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    rectangle[slideIndex - 1].className += " active";
     setTimeout(showSlides, 2000);
 }
 showSlides();
